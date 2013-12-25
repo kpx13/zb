0f5e642678b50a03ab2217e46c54f30555980dd1
Ann Kpx <annkpx@gmail.com>
2013-12-25 12:50:11 +0400

    культура сознания

--- site_static/js/script.js
+++ site_static/js/script.js
@@ -4,4 +4,19 @@ $(function () {
         $link.removeClass("active");
         $(this).addClass("active");
     });
+    
+    $('a.hide-show').click(function(){
+        par = $(this).parent();
+        console.log(par);
+        content = par.find('.content-container');
+        console.log(content);
+        if (content.hasClass('hide')) {
+            content.removeClass('hide');
+            $(this).text('Свернуть');
+        }
+        else {
+            content.addClass('hide');
+            $(this).text('Развернуть');
+        };
+    });
 });
